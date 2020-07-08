fb8dda17ecfd466c0a0b7c03131e2a523501c933
mintra2353 <ita5904@ccollege.ac.th>
2020-07-08 15:34:05 +0700

    start

---
+++ pages/ex01.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Hello: {{ name }}</h2>
+    <h2>age: {{ age }}</h2>
+    <input type="text" :value="name">
+    <input type="text" v-model="name">
+    <div><button @click="doSave">SAVE</button></div>
+    <div><button @click="doMENU">MAIN MENU</button></div>
+    <div><button @click="doHome">HOME</button></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "Mintra Jitkote",
+      age: 33,
+    }
+  },
+  methods:{
+    doSave(){
+    console.log("DO SAVE");
+    },
+    doMENU(){
+    this.$router.push('menu_act')
+   },
+   doHome(){
+    this.$router.push('/')
+   } 
+  },
+}
+</script>
